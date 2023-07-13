b6d7d6911f0d888de8c36add1dd403e5c094aa2b
Tai Nguyen <nphattai98@gmail.com>
2023-07-13 18:04:57 +0700

    feat: create order

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'nest-grpc-order',
+      username: 'infina',
+      password: 'infina',
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true, // never true in production!
+    }),
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/order/order.controller.ts
@@ -0,0 +1,15 @@
+import { Controller } from '@nestjs/common';
+import { OrderService } from './order.service';
+import { CreateOrderRequestDto } from './order.dto';
+import { GrpcMethod } from '@nestjs/microservices';
+import { ORDER_SERVICE_NAME } from './order.pb';
+
+@Controller('order')
+export class OrderController {
+  constructor(private readonly service: OrderService) {}
+
+  @GrpcMethod(ORDER_SERVICE_NAME, 'createOrder')
+  async createOrder(payload: CreateOrderRequestDto) {
+    return this.service.createOrder(payload);
+  }
+}
